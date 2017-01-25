3e6f430050c4824cac31384cb02b64c6687a7752
Vitalii27 <v.khlystun01@gmail.com>
2017-01-25 11:57:55 +0200

    homework15

---
+++ js/main.js
@@ -0,0 +1,67 @@
+
+
+
+const users = {
+  contacts: [
+    { name: 'Иван', lastName: 'Иванов', email: 'ivanov@mail.ru' },
+    { name: 'Сергей', lastName: 'Сергеев', email: 'serg@mail.ru' },
+    { name: 'Александр', lastName: 'Александров', email: 'alex@mail.ru' },
+    { name: 'Мария', lastName: 'Иванова', email: 'mary@mail.ru' },
+    { name: 'Лена', lastName: 'Иванова', email: 'len@mail.ru' }
+  ],
+  render() {
+    const body = document.body;
+    const header = this.new('header')
+    const h2 = this.new('h2')
+    const main = this.new('main');
+    const div = this.new('div');
+    const table = this.new('table');
+    const thead = this.new('thead');
+    table.append(thead);
+
+    this.contacts.forEach(contacts => {
+      const thead = this.new('thead');
+      const tr = this.new('tr');
+      table.append(thead);
+      thead.append(tr);
+      for (let key in contacts) {
+        const th = this.new('th');
+        tr.append(th);
+        th.textContent = key;
+      }
+
+    })
+
+
+    this.contacts.forEach(contacts => {
+      const tbody = this.new('tbody');
+      const tr = this.new('tr');
+      table.append(tbody);
+      tbody.append(tr);
+      for (let key in contacts) {
+        const th = this.new('th');
+        tr.append(th);
+        th.textContent = contacts[key];
+      }
+
+    })
+
+
+    body.append(header, main)
+    header.append(h2)
+    h2.textContent = 'Contacts'
+    h2.setAttribute('align', 'center')
+    main.append(div);
+    div.append(table);
+  },
+
+  new(elemName) {
+    return document.createElement(elemName);
+  },
+}
+
+
+users.render();
+
+
+
